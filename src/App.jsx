fb80a186775bb6c61da8f821caf4a04df76d1371
FlorenciaCampos <florenciacampos460@gmail.com>
2025-09-29 16:53:55 -0300

    se agrega datos de usuarios en aoo.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,23 @@ function App() {
 
   return (
     <>
-   
+     <h1>Usuarios</h1>
+    {loading ?(
+     
+      <p>Cargando usuarios</p>
+    ):(
+      users.map((user) =>{
+        
+        <div id={user._id}> {/* el id se coloca en cada elemento que se va a repetir por cada registro */}
+          <h2>{user.name}</h2>
+          <h2>{user.lastName}</h2>
+          <h3>{user.email}</h3>
+         <h4> {user.age}</h4>
+
+        </div>
+
+      })
+    )}
     
     </>
   )
